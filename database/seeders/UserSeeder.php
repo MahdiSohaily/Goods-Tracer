752f69942c2b9d117931ca34d9ffe0637b7bec18
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-12 11:42:43 +0430

    Add database migration files

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'mahdi',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin'),
+        ]);
+    }
+
+}
